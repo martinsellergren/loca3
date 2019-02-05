348886779a0e988be6ca272e519e30ad51f844b2
Martin Sellergren <martin.sellergren@gmail.com>
2019-02-05 16:21:49 +0100

    postgres query, progress. TODO: also remove wiki-duplicates

--- nom_query.sql
+++ nom_query.sql
@@ -45,3 +45,28 @@ where
 
     geometry,
     name
+
+
+WITH summary as (
+     select *, ROW_NUMBER() OVER (PARTITION BY osm_type,osm_id ORDER BY importance) as index
+     from placex)
+select
+    left(name->'name', 30),
+    place_id,
+    rank_search,
+    'https://www.openstreetmap.org/' ||
+                                     case when osm_type='N' then 'node'
+                                          when osm_type='W' then 'way'
+                                          else 'relation'
+                                     end
+                                     || '/' || osm_id,
+    coalesce(importance, 0.75-(rank_search*1.0/40))
+from summary
+where index = 1 AND name->'name' != ''
+order by
+      coalesce(importance, 0.75-(rank_search*1.0/40)) desc;
+
+
+test tar bort kopior:
+osm_id=2401213448
+select osm_type,osm_id from placex group by osm_type,osm_id having count(*) > 1;
