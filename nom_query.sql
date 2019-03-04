e4b5276b990f7b121e21fc715b2bfa5fd2f222bc
Martin Sellergren <martin.sellergren@gmail.com>
2019-03-04 18:34:16 +0100

    refractored nom-query draft

--- nom_query.sql
+++ nom_query.sql
@@ -1,89 +1,58 @@
--- Group by id. Add index based on importance in each group. Give elems data from all elems in same group.
-with aux1 as (
-select *,
-     row_number() over ordered_by_importance as index,
-     array_agg(class) over ordered_by_place_id as classes,
-     array_agg(type) over ordered_by_place_id as types,
-     array_agg(admin_level) over ordered_by_place_id as admin_levels,
-     array_agg(geometry) over ordered_by_importance as geometries
+
+with aux as (
+select
+    coalesce(importance, 0.75-(rank_search*1.0/40)) as importance,
+    row_number() over same_ids as same_ids_index,
+    row_number() over same_wikidata as same_wikidata_index,
+    row_number() over same_wikipedia as same_wikipedia_index,
+    array_agg(class) over same_ids ||
+       array_agg(class) over same_wikidata ||
+       array_agg(class) over same_wikipedia
+       as classes,
+    array_agg(type) over same_ids ||
+       array_agg(type) over same_wikidata ||
+       array_agg(type) over same_wikipedia
+       as types,
+    array_agg(admin_level) over same_ids ||
+       array_agg(admin_level) over same_wikidata ||
+       array_agg(admin_level) over same_wikipedia
+       as admin_levels,
+    array_agg(geometry) over same_ids ||
+       array_agg(geometry) over same_wikidata ||
+       array_agg(geometry) over same_wikipedia
+       as geometries,
+    array_agg(osm_type::text || ':' || osm_id) over same_ids ||
+       array_agg(osm_type::text || ':' || osm_id) over same_wikidata ||
+       array_agg(osm_type::text || ':' || osm_id) over same_wikipedia
+    as osm_ids
 from placex
 where name != ''
-window ordered_by_importance as (
-            partition by osm_type,osm_id
-            order by coalesce(importance, 0.75-(rank_search*1.0/40)) desc
-            range between unbounded preceding and unbounded following),
-     ordered_by_place_id as (
-            partition by osm_type,osm_id
-            order by place_id
-            range between unbounded preceding and unbounded following)),
+window same_ids as (
+       partition by osm_type, osm_id
+       order by coalesce(importance, 0.75-(rank_search*1.0/40)) desc, place_id
+       range between unbounded preceding and unbounded following),
+same_wikidata as (
+       partition by coalesce(extratags->'wikidata', id)
+       order by coalesce(importance, 0.75-(rank_search*1.0/40)) desc, place_id
+       range between unbounded preceding and unbounded following),
+same_wikipedia as (
+       partition by coalesce(wikipedia, id)
+       order by coalesce(importance, 0.75-(rank_search*1.0/40)) desc, place_id
+       range between unbounded preceding and unbounded following))
 
--- Filter out most important elems of each group. Select relevant data for those.
-aux2 as (
 select
-    name->'name' as name,
-    coalesce(importance, 0.75-(rank_search*1.0/40)) as importance,
+    row_number() over (order by importance) as popindex,
+    name,
     classes,
     types,
     admin_levels,
     geometries,
-    osm_type::text || ':' || osm_id as id,
-    extratags->'wikidata' as wikidata,
-    wikipedia
-from aux1
-where index = 1),
-
--- Group by wikidata.
-aux3 as (
-select
-    name,
-    importance,
-    wikipedia,
-    row_number() over ordered_by_importance as index,
-    array_agg(classes) over ordered_by_id as classes,
-    array_agg(types) over ordered_by_id as types,
-    array_agg(admin_levels) over ordered_by_id as admin_levels,
-    array_agg(geometries) over ordered_by_id as geometries,
-    array_agg(id) over ordered_by_importance as ids
-from aux2
-window ordered_by_importance as (
-            partition by coalesce(wikidata, id)
-            order by importance desc
-            range between unbounded preceding and unbounded following),
-ordered_by_id as (
-            partition by coalesce(wikidata, id)
-            order by id
-            range between unbounded preceding and unbounded following)),
-
--- Filter wikidata and group by wikipedia.
-aux4 as (
-select
-    name,
-    importance,
-    row_number() over ordered_by_importance as index,
-    array_agg(classes) over ordered_by_id as classes,
-    array_agg(types) over ordered_by_id as types,
-    array_agg(admin_levels) over ordered_by_id as admin_levels,
-    array_agg(geometries) over ordered_by_id as geometries,
-    array_agg(ids) over ordered_by_importance as ids
-from aux3
-where index = 1
-window ordered_by_importance as (
-            partition by coalesce(wikipedia, array_to_string(ids,','))
-            order by importance desc
-            range between unbounded preceding and unbounded following),
-ordered_by_id as (
-            partition by coalesce(wikipedia, array_to_string(ids,','))
-            order by array_to_string(ids,',')
-            range between unbounded preceding and unbounded following))
-
--- Filter wikipedia and final return (unnested arrays)
-select
-    name,
-    string_to_array(array_to_string(classes, '|'), '|') as classes,
-    string_to_array(array_to_string(types, '|'), '|') as types,
-    string_to_array(array_to_string(admin_levels, '|'), '|') as admin_levels,
-    string_to_array(array_to_string(geometries, '|'), '|') as geometries,
-    string_to_array(array_to_string(ids, '|'), '|') as ids
-from aux4
-where index = 1
-order by importance desc;
+    osm_ids
+from
+    aux
+where
+    same_ids_index = 1 and
+    same_wikidata_index = 1 and
+    same_wikipedia_index = 1
+order by
+      importance desc;
