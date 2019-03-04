41b048ea9612a6e6ce98351d7fd059e053ee7623
Martin Sellergren <martin.sellergren@gmail.com>
2019-03-04 17:42:37 +0100

    dedupe query draft fix

--- loca_dedupe_query.sql
+++ loca_dedupe_query.sql
@@ -3,21 +3,33 @@ CREATE EXTENSION unaccent;
 -- Add cluster-index. Clusters of proximate elements with same names.
 with aux1 as (
 select *,
-    ST_ClusterDBSCAN(geometry, eps := 50, minpoints := 1)
-                               over (partition by name) AS cid
+    ST_ClusterDBSCAN(geometry, eps := 50, minpoints := 1) over same_names AS cid
 from elems
 where supercat = 'r'
 window same_names as (
             partition by unaccent(name)
             range between unbounded preceding and unbounded following))
 
--- Aggregate clusters.
-select
-    min(popindex) as popindex,
-    name filter(where popindex=min(popindex)) as name,
-    supercat filter(where popindex=min(popindex)) as supercat,
-    subcat filter(where popindex=min(popindex)) as subcat,
-    array_agg(geometry) as geometries,
-    array_agg(osm_id) as osm_ids
+-- Group by name and cluster-id. Add index based on importance in each group. Give data from all elems in same group.
+with aux2 as (
+select *,
+    row_number() over same_clusters as index,
+    array_agg(geometry) over same_clusters as geometries,
+    array_agg(osm_id) over same_clusters as osm_ids
 from aux1
-group by unaccent(name), cid;
+window same_clusters as (
+       partition by unaccent(name), cid
+       order by popindex
+       range between unbounded preceding and unbounded following))
+
+-- Filter out most important elem, with additional data.
+select
+    popindex,
+    name,
+    supercat,
+    subcat,
+    geometries,
+    osm_ids
+from aux2
+where index = 1
+order by popindex;
